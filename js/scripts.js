0eabe1ab0a1d8a34d672856deede9df27a26c6f2
vanishady <verypeppers@yahoo.com>
2024-03-11 22:14:44 +0100

    child div better chill

--- js/scripts.js
+++ js/scripts.js
@@ -23,16 +23,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-/*
-document.addEventListener("DOMContentLoaded", function () {
-    // Load common elements dynamically
-    var sidebar = document.querySelector(".sidebar");
-    sidebar.innerHTML = `
-        <a href='#bio' style="background: rgba(36, 14, 239, 0.7);">Bio</a>
-        <a href='#coding' style="background:rgba(141, 60, 250, 0.7)">Coding</a>
-        <a href='#music' style="background:rgba(237, 64, 245, 0.7)">Music</a>
-        <a href='#contacts' style="background:rgba(253, 33, 176, 0.7)">Contacts</a>
-    `;
-});*/
+
 
 
